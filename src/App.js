bb6b071fc3d6c2e9590564b4eaf8ac6ab3887a54
zzzzzuuuuu <agu0830@naver.com>
2023-07-29 23:00:33 +0900

    style: GlobalStyle 적용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Main from './pages/Main/Main';
 import React from 'react';
 
 import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './styles/Theme';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
       <Header />
-    </>
+    </ThemeProvider>
   );
 }
 
