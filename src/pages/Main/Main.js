154923cec9b2274c663b5db9360cd1d17b7aabee
zzzzzuuuuu <agu0830@naver.com>
2023-07-30 20:58:02 +0900

    style: 배너 옆 버튼 생성

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import RightArrow from '../../assets/images/RightArrow.svg';
+import LeftArrow from '../../assets/images/LeftArrow.svg';
 
 const Main = () => {
   return (
     <>
       <MainBox>
+        <PrevBtn src={LeftArrow} />
         <MainBanner>
           서비스 소개, 공지사항, 도움말, ... 대회소개 등 배너 내용 들어갈 예정!
         </MainBanner>
+        <NextBtn src={RightArrow} />
       </MainBox>
     </>
   );
@@ -27,8 +31,17 @@ const MainBanner = styled.div`
   width: 733px;
   height: 305px;
   border-radius: 20px;
-  background-color: #ffc5da;
+  background-color: #badeff;
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 0 5px rgb(0, 0, 0, 0.3);
+`;
+
+const PrevBtn = styled.img`
+  margin-right: 27px;
+`;
+
+const NextBtn = styled.img`
+  margin-left: 27px;
 `;
