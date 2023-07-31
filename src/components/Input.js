1252236e3ad73c9be3bed51bc927d127c9747ee1
ahyexng <kay561834@gmail.com>
2023-07-31 11:06:39 +0900

    feat: 회원가입페이지

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,35 @@
 import { styled } from 'styled-components';
 
-const Input = () => {
+const Input = ({ inputType, inputName, inputMsg }) => {
   return (
     <>
       <InputFrame>
-        <InputFilled></InputFilled>
+        <InputFilled type={inputType} placeholder={inputName}></InputFilled>
       </InputFrame>
+      <InputText>{inputMsg}</InputText>
     </>
   );
 };
 
 const InputFrame = styled.div`
-  background-color: green;
-  display: flex;
-  justify-content: center;
-`;
-const InputFilled = styled.input`
   background-color: #eaeaea;
   height: 65px;
   width: 450px;
+  display: flex;
+  padding: 10px;
+  margin-bottom: 8px;
+`;
+const InputFilled = styled.input`
+  background-color: #eaeaea;
+  font-size: 20px;
+  border-style: none;
+  outline: none;
+`;
+const InputText = styled.p`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.GRAY2};
+  margin-bottom: 20px;
+  align-self: flex-start;
+  margin-left: 80px;
 `;
 export default Input;
--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -3,15 +3,30 @@ import Input from '../../components/Input';
 import ButtonLong from '../../components/ButtonLong';
 
 const Join = () => {
+  const inputMsg = {
+    emil: '사용하실 이메일을 입력해주세요.',
+    password:
+      '영문과 숫자, 특수기호를 조합하여 8~14 글자 미만으로 입력해주세요.',
+    verifyPwd: '입력하신 비밀번호를 한번 더 입력해주세요.',
+  };
   return (
     <>
       <Header />
       <JoinFrame>
         <JoinBox>
-          <h1>로그인</h1>
-          <InputFilled />
-          <InputFilled />
-          <LongButton />
+          <h1>회원가입</h1>
+          <Input inputType='text' inputName='이메일' inputMsg={inputMsg.emil} />
+          <Input
+            inputType='password'
+            inputName='비밀번호'
+            inputMsg={inputMsg.password}
+          />
+          <Input
+            inputType='password'
+            inputName='비밀번호 확인'
+            inputMsg={inputMsg.verifyPwd}
+          />
+          <ButtonLong btnName='회원가입' />
           <p></p>
         </JoinBox>
       </JoinFrame>
@@ -24,14 +39,21 @@ const Header = styled.div`
 `;
 
 const JoinFrame = styled.div`
-  height: 659px;
+  height: 660px;
   display: flex;
   justify-content: center;
-  align-items: center;
 `;
 const JoinBox = styled.div`
-  height: 480px;
+  height: 620px;
   width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   box-shadow: rgb(0, 0, 0, 0.15) 0px 5px 10px 0px;
+  h1 {
+    font-size: 28px;
+    font-weight: 700;
+    margin: 50px;
+  }
 `;
 export default Join;
