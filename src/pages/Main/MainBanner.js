c8bc836efaaef50339625a631c1a93a498063cd5
zzzzzuuuuu <agu0830@naver.com>
2023-07-31 23:49:53 +0900

    feat: 배너슬라이드 분리

---
+++ src/pages/Main/MainBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Swiper } from 'swiper';
+
+const MainBanner = () => {
+  return (
+    <div>
+      <Swiper></Swiper>
+    </div>
+  );
+};
+
+export default MainBanner;
