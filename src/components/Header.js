cd1d3eb7a332eb296f0fa851d632cfa8fb3c6ee7
ahyexng <kay561834@gmail.com>
2023-08-04 17:35:54 +0900

    refactor: header onclick 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,10 +27,11 @@ const HeaderWrap = styled.div`
 const HeaderBar = styled.div`
   width: 100%;
   height: 63px;
-  box-shadow: 0 0 3px black;
+  box-shadow: 0 0 5px black;
   display: flex;
   align-items: center;
 `;
+
 const Logo = styled.button`
   width: 99px;
   height: 49px;
@@ -39,6 +40,7 @@ const Logo = styled.button`
   margin: 7px 0 7px 22px;
   border-style: none;
 `;
+
 const Learning = styled.button`
   margin-left: 79px;
   font-size: 20px;
@@ -46,12 +48,14 @@ const Learning = styled.button`
   border-style: none;
   background-color: white;
 `;
+
 const SignBox = styled.div`
   margin-left: auto;
   margin-right: 67px;
   display: flex;
   margin-top: 20px;
 `;
+
 const LoginBtn = styled.button`
   height: 27px;
   width: 90px;
@@ -60,4 +64,5 @@ const LoginBtn = styled.button`
   border-style: none;
   background-color: white;
 `;
+
 const JoinBtn = styled(LoginBtn)``;
