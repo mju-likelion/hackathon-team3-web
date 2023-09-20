f91f6d3a6aa5e267b6d46dd883d428827c42f6dc
zzzzzuuuuu <agu0830@naver.com>
2023-09-20 22:43:19 +0900

    feat: 관리자 페이지 추가

---
+++ src/pages/Admin/AdminPage.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const AdminPage = () => {
+  return (
+    <>
+      <AdminPageBox>여기는 어드민 페이지 입니다. . . .</AdminPageBox>
+    </>
+  );
+};
+
+export default AdminPage;
+
+const AdminPageBox = styled.div`
+  width: 100%;
+`;
